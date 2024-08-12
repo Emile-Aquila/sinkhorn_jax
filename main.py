507932a094463188236f92d46ab0b38278f05858
Emile-Aquila <tmk8oakan6130@gmail.com>
2024-08-12 20:59:55 +0900

    update plot

--- main.py
+++ main.py
@@ -26,29 +26,45 @@ if __name__ == '__main__':
             jnp.ones(y.shape[0]) / y.shape[0],
             d_euclidean_matrix(x, y),
             epsilon
-        )[0]
+        )
     )
-    vg_pc2wd = jit(jax.value_and_grad(f_pc2wd, argnums=0, has_aux=False))  # point cloud to wasserstein distance
+    vg_pc2wd = jit(jax.value_and_grad(lambda x, y: f_pc2wd(x, y)[0], argnums=0, has_aux=False))  # point cloud to wasserstein distance
+    f_pc2transport = jit(lambda x, y: f_pc2wd(x, y)[1])
 
-    costs, source_points_list = [], []
+    costs, source_points_list, matrixs = [], [], []
     for _ in range(500):  # optimization loop
-        cost, grad = vg_pc2wd(source_points, target_points)
+        cost_tmp, grad = vg_pc2wd(source_points, target_points)
+        matrix = f_pc2transport(source_points, target_points)
         source_points = source_points - 0.1 * grad
-        costs.append(cost)
+        costs.append(cost_tmp)
+        matrixs.append(matrix)
         source_points_list.append(source_points)
 
     print(f"calculation finished, wasserstein dist: {costs[-1]}")
 
-    fig, ax = plt.subplots()
-    def update_anim(source_tmp):
-        ax.cla()
-        ax.scatter(source_tmp[:, 0], source_tmp[:, 1], c='b', label="source")
-        ax.scatter(source_points_start[:, 0], source_points_start[:, 1], c='g', label="source_start", marker="o")
-        ax.scatter(target_points[:, 0], target_points[:, 1], c='r', label="target", marker="x")
-        ax.legend()
-        return ax
+    fig, ax, bx = plt.figure(figsize=(12, 7)), plt.subplot(121), plt.subplot(122)
 
-    anim = FuncAnimation(fig, update_anim, frames=source_points_list, interval=20, cache_frame_data=False)
+    def update_anim(inputs):
+        source_tmp, p, cost = inputs
+        ax.cla()
+        bx.cla()
+
+        ax.imshow(p)
+        ax.set_title("transport matrix")
+        ax.set_aspect('equal')
+
+        bx.scatter(source_tmp[:, 0], source_tmp[:, 1], c='b', label="source")
+        bx.scatter(source_points_start[:, 0], source_points_start[:, 1], c='g', label="source_start", marker="o")
+        bx.scatter(target_points[:, 0], target_points[:, 1], c='r', label="target", marker="x")
+        bx.set_title(f"cost: {cost:.3f}")
+        bx.set_aspect('equal')
+
+        # ax.legend()
+        bx.legend()
+        fig.tight_layout()
+        return ax, bx
+
+    anim = FuncAnimation(fig, update_anim, frames=zip(source_points_list, matrixs, costs), interval=20, cache_frame_data=False)
     anim.save("output.gif", writer="pillow")
     plt.close()
 
